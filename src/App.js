39d99c7b6db16a068337a51d2d2500fca0f8a46c
imbrikis <imbrikis@yahoo.com>
2021-06-16 13:45:04 -0600

    example: simple counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    default:
+      return state
+  }
+}
 
 const App = () => {
+  const [count, dispatch] = useReducer(reducer, 0)
+
   return (
     <div>
-      <h1>Learning useReducer</h1>
+      <h1>count: {count}</h1>
+      <button onClick={() => dispatch({ type: 'increment' })}>increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>increment</button>
     </div>
   )
 }
